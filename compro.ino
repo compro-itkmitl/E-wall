402935c5d4e34ea65face3dd10e04dedacb1b195
focus <fogus251@hotmail.com>
2018-04-26 14:15:23 +0700

    bug fix and remove servo

--- compro.ino
+++ compro.ino
@@ -1,18 +1,17 @@
 #include "Arduino.h"
 #include "SoftwareSerial.h"
 #include "DFRobotDFPlayerMini.h"
-#include "Servo.h"
 #include "LedControl.h"
 byte hf[3][8]= {{B01000010,B11100111,B11111111,B11111111,B11111111,B01111110,B00111100,B00011000}
-               ,{B00000000,B00000000,B11111111,B11111111,B11111111,B01111110,B00000000,B00000000}
+               ,{B00000000,B00000000,B01111110,B01111110,B01111110,B01111110,B00000000,B00000000}
                ,{B00000000,B00000000,B00000000,B11111111,B11111111,B00000000,B00000000,B00000000}
 };
 byte nf[3][8]={{B00111100,B01111110,B11111111,B11100111,B11100111,B11111111,B01111110,B00111100}
               ,{B00111100,B01111110,B11111111,B11111111,B11100111,B11111111,B01111110,B00111100}, 
                {B00111100,B01111110,B11111111,B11111111,B11111111,B11111111,B01111110,B00111100}
 };
-byte sf1[8]={B11111111,B11111111,B00111000,B00111000,B00011100,B00011100,B00111000,B00111000};
-byte sf2[8]={B11111111,B11111111,B00011100,B00011100,B00111000,B00111000,B00011100,B00011100};
+byte sf[2][8]={{B11111111,B11111111,B00111000,B00111000,B00011100,B00011100,B00111000,B00111000}
+              ,{B11111111,B11111111,B00011100,B00011100,B00111000,B00111000,B00011100,B00011100}};
 byte zf1[8]={B11111111,B01000000,B00100000,B00010000,B00001000,B00000100,B00000010,B11111111};
 byte zf2[8]={B11111111,B11000000,B01100000,B00110000,B00011000,B00001100,B00000110,B11111111};
 byte zf3[8]={B11111111,B11111111,B01110000,B00111000,B00011100,B00001110,B11111111,B11111111};
@@ -24,18 +23,16 @@ byte alf[3][8]={{B00001100,B00011110,B00111111,B01100111,B11100111,B11111111,B01
                 ,{B00001100,B00011110,B00111111,B01111111,B11100111,B11111111,B01111110,B00111100}
                 ,{B00001100,B00011110,B00111111,B01111111,B11111111,B11111111,B01111110,B00111100}
 };
-
+byte x[8]={B10000001,B01000010,B00100100,B00011000,B00011000,B00100100,B01000010,B10000001};
 byte startl[8]={B00111100,B00000100,B00000100,B00111100,B00000100,B00000100,B00000100,B00111100};
 byte startr[8]={B10000001,B10000001,B10000001,B10011001,B10100101,B10100101,B10100101,B01000010};
 SoftwareSerial mySoftwareSerial(10, 11);
-Servo head;
-Servo base;
 DFRobotDFPlayerMini sound;
-int j=2, mood=1, command=0, frame, animation=0;
+int j=2, mood=1, command=0, animation=0;
 int vol=25;
 char wheel;
 long duration,cm,inches;
-unsigned long timer;
+unsigned long timer,frame,now,song;
 int state=0; 
 int enA=9,enB=3,in1=8,in2=7,in3=4,in4=2,trig=6,echo=5;
 LedControl lc=LedControl(13,A0,12,2);//DIN,CLK,CS,num of display
@@ -53,13 +50,7 @@ void setup() {
   mySoftwareSerial.begin(9600);
   Serial.begin(9600);
 
-  //servo setup
-  head.attach(A1);
-  base.attach(A2);
-  head_go(90);
-  delay(39);
-  base_go(100);
-  delay(39);
+ 
   //led setup
   lc.shutdown(0,false); //start display
   lc.setIntensity(0,0);// Set brightness
@@ -77,7 +68,6 @@ void setup() {
     Serial.println(F("Unable to begin:"));
     Serial.println(F("1.Please recheck the connection!"));
     Serial.println(F("2.Please insert the SD card!"));
-    while(true);
   }
   Serial.println(F("DFPlayer Mini online."));
   for(int i=0;i<8;i++){
@@ -85,18 +75,20 @@ void setup() {
     lc.setColumn(1,i,startr[i]);
     }
   sound.volume(25);  //Set volume value6 From 0 to 30
-  sound.play(6);
-  delay(5000);
-  timer = millis();
-  frame = millis();
+  sound.play(9);
+  delay(6000);
+  timer = now;
+  frame = now;
+  song = now;
   
 } 
 void loop() {
+  now = millis();
   if(Serial.available() > 0){ // Checks whether data is comming from the serial port
     command = Serial.read(); // Reads the data from the serial port
  }
-  if (millis() - timer >= 60000){//timer of mood 
-    timer = millis();
+  if (now - timer >= 15000){//timer of mood 
+    timer = now;
     mood--;
     if (mood==1){
     Serial.println("E-wall mood is normal");
@@ -118,7 +110,7 @@ void loop() {
   if (command == '1') {
  
     mood++;
-    timer=millis();
+    timer=now;
     if (mood==1){
     Serial.println("E-wall mood is normal");
     }
@@ -150,22 +142,51 @@ void loop() {
       }
     command=0;
     }
+  if(command=='3'){
+     Serial.println("stop");
+    }
+  while(command =='3'){
+      if(Serial.available()>0){
+          command =Serial.read();
+        }
+      digitalWrite(in4,LOW);
+      digitalWrite(in3,LOW);
+      analogWrite(enB,255);
+      digitalWrite(in1,LOW);
+      digitalWrite(in2,LOW);
+      analogWrite(enA,255);
+      sonic();
+    }
   if (mood == 0) {
     sad();
-    sound.play(2);
+    if(now-frame>=150){
+      frame=now;
+      if(animation==1){
+          animation=0;
+        }
+      else{
+          animation=1;
+        }
+    }
+    if(now-song>=13000){
+      sound.play(2e  );
+    song=now;
+    }
    
   }
   else if(mood<=-1){
     mood =-1;
     sleep();
     wheel = 's';
+    if(now-song>=13000){
+      song=now;
     sound.play(1);
+    }
     }
   else if(mood==1){
     normal();
-     if (millis()-frame>=50){
-      frame =millis();
-
+     if (now-frame>=250){
+      frame =now;
       if(animation< j){
         animation++;
         j=2; 
@@ -175,12 +196,15 @@ void loop() {
         animation--;
        }  
       }
-
+  if(now-song>=13000){
+    song=now;
     sound.play(3);
+    }
   }
   else if(mood==2){
     happy();
-    if(millis()-frame>=100){
+    if(now-frame>=250){
+      frame=now;
       if(animation==1){
           animation=0;
         }
@@ -188,13 +212,14 @@ void loop() {
           animation=1;
         }
     }
-    sound.play(4);
+    sound.play(12);
+    song=now;
     }
   else if(mood>=3){
     //angry
     angry();
-    if (millis()-frame>=10){
-      frame=millis();
+    if (now-frame>=150){
+      frame=now;
       if(animation<j){
         animation++;
         j=2;}
@@ -202,33 +227,35 @@ void loop() {
         j=1;
         animation--;}
       }
-    sound.play(5);
+    if(now-song>=13000){
+      sound.play(10);
+      song=now;
+    }
     mood=3;
     }
   if(wheel=='s'){
-  digitalWrite(in4,HIGH);
-  digitalWrite(in3,HIGH);
+  digitalWrite(in4,LOW);
+  digitalWrite(in3,LOW);
   analogWrite(enB,255);
-  digitalWrite(in1,HIGH);
-  digitalWrite(in2,HIGH);
+  digitalWrite(in1,LOW);
+  digitalWrite(in2,LOW);
   analogWrite(enA,255);
-  Serial.println('s');
   }
   else if (wheel == 'f') {
   digitalWrite(in4,HIGH);
   digitalWrite(in3,LOW);
-  analogWrite(enB,255);
-  digitalWrite(in1,HIGH);
-  digitalWrite(in2,LOW);
-  analogWrite(enA,255);
+  analogWrite(enB,60);
+  digitalWrite(in1,LOW);
+  digitalWrite(in2,HIGH);
+  analogWrite(enA,60);
   wheel = 0;
  }
  else if (wheel == 'b') {
   digitalWrite(in4,LOW);
   digitalWrite(in3,HIGH);
   analogWrite(enB,255);
-  digitalWrite(in1,LOW);
-  digitalWrite(in2,HIGH);
+  digitalWrite(in1,HIGH);
+  digitalWrite(in2,LOW);
   analogWrite(enA,255);
   wheel = 0;
  }
@@ -242,12 +269,12 @@ void loop() {
   wheel = 0;
  } 
   else if (wheel == 'l') {
-  digitalWrite(in4,HIGH);
-  digitalWrite(in3,LOW);
-  analogWrite(enB,255);
+  digitalWrite(in4,LOW);
+  digitalWrite(in3,HIGH);
+  analogWrite(enB,60);
   digitalWrite(in1,LOW);
   digitalWrite(in2,LOW);
-  analogWrite(enA,255);
+  analogWrite(enA,60);
   wheel = 0;
  } 
 }
@@ -263,49 +290,18 @@ void sonic(){
   duration = pulseIn(echo, HIGH);
   // convert the time into a distance
   cm = (duration/2) / 29.1;
-  if (cm<10){
+  
+  if (cm<30){
   wheel = 'l';
-  base_go(150);
   }
-  else if (cm>=10){
+  else if (cm>=30){
   wheel = 'f';
-  base_go(90);
   }
   
 }
 
 
 
-void base_go(int finalAngle){
-  int currentAngle = base.read();
-  if (finalAngle>currentAngle){
-    for(int i= currentAngle;i<finalAngle;i++){
-        base.write(i);
-        delay(10);
-      }
-    }
-   else{
-    for(int i=currentAngle; i >finalAngle;i--){
-        base.write(i);
-        delay(10);
-      }
-    }
-  }
-void head_go(int finalAngle){
-  int currentAngle = base.read();
-  if (finalAngle>currentAngle){
-    for(int i= currentAngle;i<finalAngle;i++){
-        head.write(i);
-        delay(10);
-      }
-    }
-   else{
-    for(int i=currentAngle; i >finalAngle;i--){
-        head.write(i);
-        delay(10);
-      }
-    }
-  }  
 //check sd card detail
 void printDetail(uint8_t type, int value){
   switch (type) {
@@ -370,13 +366,8 @@ void happy(){
   
 void sad(){
   for(int i=0;i<8;i++){
-      lc.setColumn(0,i,sf1[i]);
-      lc.setColumn(1,i,sf1[i]);
-  }
-  
-  for(int i=0;i<8;i++){
-      lc.setColumn(0,i,sf2[i]);
-      lc.setColumn(1,i,sf2[i]);
+      lc.setColumn(0,i,sf[animation][i]);
+      lc.setColumn(1,i,sf[animation][i]);
   }
         
   }
@@ -393,8 +384,7 @@ void sleep(){
       lc.setColumn(0,i,zf1[i]);
       lc.setColumn(1,i,zf1[i]);
     }
-  base_go(100);
-  head_go(90);
+
   delay(500);
   for(int i=0;i<8;i++){
       lc.setColumn(0,i,zf2[i]);
@@ -405,7 +395,6 @@ void sleep(){
       lc.setColumn(0,i,zf3[i]);
       lc.setColumn(1,i,zf3[i]);
     }
-  head_go(115);
   delay(500);
    timer+=1500;
   }
